836eb3a03bbd30d88385d26b918d9f450fa4db7e
Rahul <rahul.bh@sensesemi.com>
2024-12-23 17:27:11 +0530

    updates files

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include "portaudio.h"
+#include "captureData.h"
+//#include "fft.h"
+//#include "featureExtraction.h"
+
+
+
+int main() {
+    capture();
+//    test();
+  //  featureExtraction();
+}
